959658efc2b68d6f8d67b02324ac9562639e465a
Lance Pidor <kyrios213@gmail.com>
2021-06-17 11:22:18 +0800

    Models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='books')
+    isbn = models.CharField(max_length=20, blank=True, null=True)
+    pages = models.IntegerField()
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    stock = models.IntegerField()
+    description = models.TextField(blank=True, null=True)
+    image_url = models.ImageField(blank=True, null=True)
+    status = models.BooleanField(default=True)
+    date_created = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['date_created']
+    
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    product_tag = models.CharField(max_length=15)
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    stock = models.IntegerField()
+    image_url = models.ImageField(blank=True, null=True)
+    status = models.BooleanField(default=True)
+    date_created = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['date_created']
+
+    def __str__(self):
+        return f"{self.product_tag} {self.name}"
