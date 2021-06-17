a84067bec12fce03b5cf50a979541f14f090623d
Lance Pidor <kyrios213@gmail.com>
2021-06-17 22:58:42 +0800

    Finished

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,13 @@ from . import views
 
 urlpatterns = [
     path('', views.test, name="test"),
+
+    path('category/', views.CategoryListView.as_view(), name="category"),
+    path('category/<int:pk>', views.CategoryDetailView.as_view(), name="category_detail"),
+
+    path('book/', views.BookListView.as_view(), name="book"),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name="book_detail"),
+
+    path('product/', views.ProductListView.as_view(), name="product"),
+    path('product/<int:pk>', views.ProductDetailView.as_view(), name="product_detail"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import generics
+
+from .serializers import *
+from .models import *
 
 def test(request):
     return HttpResponse("Home")
+
+
+class CategoryListView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    
+
+class BookListView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class ProductListView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
